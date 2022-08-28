b37399ccf212a0bf59822ae2807ef8677c3c1b52
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-08-28 19:21:10 -0300

    Creating Product Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 
-import './App.css';
-import Navbar from './components/Navbar';
-import Hero from "./components/Hero"
 import { BrowserRouter as Router } from "react-router-dom"
 import { GlobalStyle } from './globalStyle.js'
-
+import Hero from "./components/Hero"
+import Products from './components/Products'
+import { productData } from "./components/Products/data.js";
 
 function App() {
   return (
     <Router>
         <GlobalStyle /> 
       <Hero/>
+      <Products heading="Choose your favorority"  data={productData}/>
     </Router>
    
   );
---
+++ src/components/Products/data.js
@@ -0,0 +1,30 @@
+import product1 from '../../images/pizza-1.jpg'
+import product2 from '../../images/pizza-2.jpg'
+import product3 from '../../images/product-3.jpg'
+export const productData = [
+    {
+        img: product1,
+        alt: 'Pizza',
+        name: 'Supreme Pizza',
+        desc: 'Marinare sauce, basil, italian sausage, roma tomatoes, olives and pesto',
+        price: '$19,90',
+        button: 'Add to Cart'
+    },
+    {
+        img: product2,
+        alt: 'Pizza',
+        name: 'Hawailian Paradise',
+        desc: 'Marinare sauce, basil, italian sausage, roma tomatoes, olives and pesto',
+        price: '$16,90',
+        button: 'Add to Cart'
+    },
+    {
+        img: product3,
+        alt: 'Pizza',
+        name: 'Vaggie Overload',
+        desc: 'Marinare sauce, basil, italian sausage, roma tomatoes, olives and pesto',
+        price: '$14,90',
+        button: 'Add to Cart'
+    },
+    
+]
